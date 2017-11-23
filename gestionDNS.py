a5d92a5caa10d2f605790fcc228950b4e5fefee6
Ferrete <sergioferretebenitez@gmail.com>
2017-11-23 02:28:12 +0100

    Deleting comments

--- gestionDNS.py
+++ gestionDNS.py
@@ -3,24 +3,14 @@ import sys
 import os
 import mmap
 
-#* ``-a`` o ``-b``: Si recibe ``-a`` añadirá un nuevo nombre,
-# si recibe ``-b`` borrará el nombre que ha recibido.
-#* ``-dir`` o ``-alias``, si recibe ``-dir`` va a crear un registro tipo A,
-# si recibe ``-alias`` va a crear un registro CNAME
-#* El nombre de la máquina para añadir o borrar
-#* El nombre del alias o la dirección ip: Si has usuado la opción ``-dir``
-#recibirá una ip y si has usuado ``-alias`` recibirá el nombre de la máquina
-# a la que le vamos a hacer el alias. Si has utilizado -b no teendrá
-# este parámetro.
-
 action=sys.argv[1]
-regtype=sys.argv[2]
-hostname=sys.argv[3]
 restartbind=os.system("systemctl restart bind9")
 directzonefile="/var/cache/bind/db.ferrete.gonzalonazareno.org"
 inversefloatzonefile="/var/cache/bind/db.172.22.200"
 inversestaticzonefile="/var/cache/bind/db.10.0.0"
 if action == '-a':
+    regtype=sys.argv[2]
+    hostname=sys.argv[3]
     iporalias=sys.argv[4]
     ip=iporalias.split(".")
     filetosearch=open(directzonefile)
@@ -67,6 +57,7 @@ if action == '-a':
             print("Need specific [IP] or [ALIAS]")
 
 elif action == '-b':
+    hostname=sys.argv[2]
     f = open(directzonefile)
     output = []
     for line in f:
